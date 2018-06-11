fbabef0c5daadd4eb98696f5719f16315b5662ee
Peter Bamberger <peter.bamberger@pushpay.com>
2018-06-12 11:54:20 +1200

    List actual attribute names in generated queries

--- src/commands/selectTop.ts
+++ src/commands/selectTop.ts
@@ -1,27 +1,50 @@
 import BaseCommand from "../common/baseCommand";
 import * as vscode from 'vscode';
-import { PostgreSQLTreeDataProvider } from "../tree/treeProvider";
+//import { PostgreSQLTreeDataProvider } from "../tree/treeProvider";
 import { TableNode } from "../tree/tableNode";
 import { EditorState } from "../common/editorState";
 import { Database } from "../common/database";
 
 export class selectTopCommand extends BaseCommand {
-  async run(treeNode: TableNode) {
-    // prompt for count
-    const countInput: string = await vscode.window.showInputBox({ prompt: "Select how many?", placeHolder: "port" });
-    if (!countInput) return;
-
-    const count: number = parseInt(countInput);
-    if (Number.isNaN(count)) {
-      vscode.window.showErrorMessage('Invalid quantity for selection - should be a number');
-      return;
+  async run(treeNode: TableNode, count: number = 0) {
+    if (count == 0) {
+      const countInput: string = await vscode.window.showInputBox({ prompt: "How many rows?", placeHolder: "port" });
+      if (!countInput) return;
+      count = parseInt(countInput);
+      if (Number.isNaN(count)) {
+        vscode.window.showErrorMessage('Invalid quantity for selection - should be a number');
+        return;
+      }
     }
-
-    const sql = `SELECT * FROM ${treeNode.getQuotedTableName()} LIMIT ${count};`
-    const textDocument = await vscode.workspace.openTextDocument({content: sql, language: 'postgres'});
-    await vscode.window.showTextDocument(textDocument);
-    EditorState.connection = treeNode.connection;
-
-    return Database.runQuery(sql, vscode.window.activeTextEditor, treeNode.connection);
+    const connection = await Database.createConnection(treeNode.connection);
+    try {
+      const query = `
+select a.attname as column_name
+from pg_attribute a
+left join pg_index primaryIndex on primaryIndex.indrelid = a.attrelid and a.attnum = any(primaryIndex.indkey) and primaryIndex.indisprimary = true
+where 
+  a.attrelid = '${treeNode.getQuotedTableName()}'::regclass
+  and a.attnum > 0
+  and not a.attisdropped
+order by a.attnum;`;      
+      const result = await connection.query(query);      
+      const columns = result.rows.map(c => `${(c.column_name === c.column_name.toLowerCase()) ? c.column_name : '"' + c.column_name + '"' }`);
+      const columnsDisplay = columns.join("\n  ,");
+      const sql = `
+select
+  ${columnsDisplay}
+from ${treeNode.getQuotedTableName()}
+limit ${count}; `;
+      const textDocument = await vscode.workspace.openTextDocument({content: sql, language: 'postgres'});
+      await vscode.window.showTextDocument(textDocument);
+      EditorState.connection = treeNode.connection;
+      return Database.runQuery(sql, vscode.window.activeTextEditor, treeNode.connection);
+    }
+    catch(err) {
+      return err;
+    }
+    finally {
+      await connection.end();
+    }
   }
 }
--- src/commands/selectTop1000.ts
+++ src/commands/selectTop1000.ts
@@ -1,3 +1,4 @@
+import { selectTopCommand } from "../commands/selectTop";
 import BaseCommand from "../common/baseCommand";
 import * as vscode from 'vscode';
 import { PostgreSQLTreeDataProvider } from "../tree/treeProvider";
@@ -5,12 +6,8 @@ import { TableNode } from "../tree/tableNode";
 import { EditorState } from "../common/editorState";
 import { Database } from "../common/database";
 
-export class selectTop1000Command extends BaseCommand {
-  async run(treeNode: TableNode) {
-    const sql = `SELECT * FROM ${treeNode.getQuotedTableName()} LIMIT 1000;`
-    const textDocument = await vscode.workspace.openTextDocument({content: sql, language: 'postgres'});
-    await vscode.window.showTextDocument(textDocument);
-    EditorState.connection = treeNode.connection;
-    return Database.runQuery(sql, vscode.window.activeTextEditor, treeNode.connection);
-  }
+export class selectTop1000Command extends selectTopCommand {
+   async run(treeNode: TableNode) {
+    return super.run(treeNode, 1000);
+   }
 }
