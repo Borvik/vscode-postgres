4df321c6158e39c8cbc56d857d9a11ace57ce8e5
Chris Kolkman <ckolkman@treorisoft.com>
2019-03-07 07:28:25 -0500

    pg11 support for language server

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -3,6 +3,7 @@
 export class SqlQueries {
   TableColumns: string;
   GetFunctions: string;
+  GetAllFunctions: string;
 
   public format(stringValue: string, ...formatParams: any[]): string {
     return stringValue.replace(/{(\d+)}/g, (match: string, number: string): string => {
@@ -34,6 +35,27 @@ let queries = {
           LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
           LEFT JOIN pg_catalog.pg_description d ON p.oid = d.objoid
       WHERE n.nspname = $1
+        AND p.prorettype <> 'pg_catalog.trigger'::pg_catalog.regtype
+        AND has_schema_privilege(quote_ident(n.nspname), 'USAGE') = true
+        AND has_function_privilege(p.oid, 'execute') = true
+      ORDER BY 1, 2, 4;`,
+    GetAllFunctions:
+      `SELECT n.nspname as "schema",
+        p.proname as "name",
+        d.description,
+        pg_catalog.pg_get_function_result(p.oid) as "result_type",
+        pg_catalog.pg_get_function_arguments(p.oid) as "argument_types",
+      CASE
+        WHEN p.proisagg THEN 'agg'
+        WHEN p.proiswindow THEN 'window'
+        WHEN p.prorettype = 'pg_catalog.trigger'::pg_catalog.regtype THEN 'trigger'
+        ELSE 'normal'
+      END as "type"
+      FROM pg_catalog.pg_proc p
+          LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+          LEFT JOIN pg_catalog.pg_description d ON p.oid = d.objoid
+      WHERE n.nspname <> 'information_schema'
+        AND pg_catalog.pg_function_is_visible(p.oid)
         AND p.prorettype <> 'pg_catalog.trigger'::pg_catalog.regtype
         AND has_schema_privilege(quote_ident(n.nspname), 'USAGE') = true
         AND has_function_privilege(p.oid, 'execute') = true
@@ -151,6 +173,27 @@ let queries = {
           LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
           LEFT JOIN pg_catalog.pg_description d ON p.oid = d.objoid
       WHERE n.nspname = $1
+        AND p.prorettype <> 'pg_catalog.trigger'::pg_catalog.regtype
+        AND has_schema_privilege(quote_ident(n.nspname), 'USAGE') = true
+        AND has_function_privilege(p.oid, 'execute') = true
+      ORDER BY 1, 2, 4;`,
+    GetAllFunctions: `
+      SELECT n.nspname as "schema",
+        p.proname as "name",
+        d.description,
+        pg_catalog.pg_get_function_result(p.oid) as "result_type",
+        pg_catalog.pg_get_function_arguments(p.oid) as "argument_types",
+      CASE
+        WHEN p.prokind = 'a' THEN 'agg'
+        WHEN p.prokind = 'w' THEN 'window'
+        WHEN p.prorettype = 'pg_catalog.trigger'::pg_catalog.regtype THEN 'trigger'
+        ELSE 'normal'
+      END as "type"
+      FROM pg_catalog.pg_proc p
+          LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+          LEFT JOIN pg_catalog.pg_description d ON p.oid = d.objoid
+      WHERE n.nspname <> 'information_schema'
+        AND pg_catalog.pg_function_is_visible(p.oid)
         AND p.prorettype <> 'pg_catalog.trigger'::pg_catalog.regtype
         AND has_schema_privilege(quote_ident(n.nspname), 'USAGE') = true
         AND has_function_privilege(p.oid, 'execute') = true
